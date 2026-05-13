d8062c75a9e49f818df4ae239dd30ca5a19984bd
THOMASterPRO <thomas@bezemer.net>
2026-05-13 16:56:51 +0200

    Add localization files for multiple languages including Czech, German, Danish, Estonian, English, Spanish, Finnish, French, Greek, Croatian, Hungarian, Irish, Italian, Lithuanian, Latvian, Maltese, Dutch, Polish, Portuguese, Romanian, Slovenian, Slovak, and Swedish.
    - Each language file contains translations for common allergens and instructions for individuals with mild and severe food allergies.
    - Update index.js to include new language options for localization.

---
+++ src/locales/index.js
@@ -0,0 +1,56 @@
+import bg from './bg';
+import cz from './cz';
+import de from './de';
+import dk from './dk';
+import ee from './ee';
+import en from './en';
+import es from './es';
+import fi from './fi';
+import fr from './fr';
+import gr from './gr';
+import hr from './hr';
+import hu from './hu';
+import ie from './ie';
+import it from './it';
+import lt from './lt';
+import lv from './lv';
+import mt from './mt';
+import nl from './nl';
+import pl from './pl';
+import pt from './pt';
+import ro from './ro';
+import si from './si';
+import sk from './sk';
+import sv from './sv';
+
+
+export const LANGUAGE_OPTIONS = {
+  en,
+  nl,
+  es,
+  it,
+  de,
+  sv,
+  pt,
+  fr,
+  bg,
+  hr,
+  cz,
+  dk,
+  ee,
+  fi,
+  gr,
+  hu,
+  ie,
+  lv,
+  lt,
+  mt,
+  pl,
+  ro,
+  sk,
+  si
+};
+
+
+
+
---
+++ src/locales/unused/ja.js
@@ -0,0 +1,33 @@
+export default {
+    iso: "jp",
+    lang: {
+        english: "Japanese",
+        divide: " | ",
+        trans: "日本語"
+    },
+    allergies: {
+        milk: "牛乳",
+        eggs: "卵",
+        tree_nuts: "ナッツ類",
+        shellfish: "貝類",
+        peanuts: "ピーナッツ",
+        fish: "魚",
+        soy: "大豆",
+        wheat: "小麦",
+        gluten: "グルテン",
+        sesame: "胡麻",
+        lupine: "ルピナス",
+        corn: "トウモロコシ",
+        meat: "肉類",
+    },
+    instructions: {
+        mild: {
+            front: "Ik heb milde voedselallergieën, Wees voorzichtig met voedsal dat het volgende bevat:",
+            back: "Het is niet nodig om al het keukengerei schoon te maken voor het bereiden van mijn voedsel, maar probeer erop te letten dat het geen stukken van de allergenen op de andere kant van deze kaart vermeldt bevat."
+        },
+        severe: {
+            front: "私には重度の食物アレルギーがあります。 生命を脅かすアレルギー反応を避けるた め、以下が含まれているすべての食品を避 ける必要があります:",
+            back: "私の食事にはこのカードに表記してある成分が含ま れていないこと、および、私の食器、食事の準備に 使用された調理器具や調理台が使用する直前にすべ て洗浄されていることをご確認ください。 ご協力ありがとうございます。"
+        }
+    }
+}
